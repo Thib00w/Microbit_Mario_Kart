7c8668f7f037c3e8324b2f3fbefcb787b8a63624
tobby180 <thyliol18@gmail.com>
2026-04-30 10:58:13 +0200

    clone,init main arbite

--- arbite/main.py
+++ arbite/main.py
@@ -1,1 +1,14 @@
 from microbit import *
+import radio
+
+#initialisation de l'emmeteur radio
+radio.config(group = 0 , power = 7)
+radio.on 
+
+Mon_id = "ARB"
+
+def envoi_message(destinataire, type, valeur):
+    lst = [type, Mon_id, destinataire, valeur]
+    msg = str(lst)
+    radio.send(msg)
+
