2772fb04a5a60a4bf442da73b629d0f201c387d4
Thibaut_com <thibaut.commeau@outlook.fr>
2026-05-05 14:01:17 +0200

    Refonde du systeme de direction à régler le probleme de marche avant et de marche arrière

--- kart/main.py
+++ kart/main.py
@@ -1,16 +1,20 @@
 from microbit import *
 import radio as rd
 
+# Identifiant pour microbit
 MON_TYPE = "KT"
 MON_ID = "KT2"
 MON_GAMEPAD = "GP2"
 ARBITRE = "ARB"
 
+# addresse des moteur de macqueen 
+ADDR = 0x10
+
 # Config radio
 rd.on()
 rd.config(group=22)
 
-def rd_décode(message: str):
+def rd_decode(message: str):
     splt_msg = message.split(';')
     if len(splt_msg) == 4:
         type_ = splt_msg[0]
@@ -25,45 +29,64 @@ def rd_envoie(dest: str, values: list):
     msg = str(MON_TYPE) + ";" + str(MON_ID) + ";" + str(dest) + ";" + str(values)
     rd.send(msg)
 
-def moteurs(x, b, a):
-    if not b and not a:
-        i2c.write(0x10, bytes([0x00, 0, 0]))
-        i2c.write(0x10, bytes([0x02, 0, 0]))
-        return
-        
-
-    vitesse_base = 100 
-
-    if x > 542:
-        coeff = (x - 542) / 481
-        gauche = vitesse_base
-        droit = int(vitesse_base * (1 - coeff))
-    elif x < 482:
-        coeff = (482 - x) / 482
-        gauche = int(vitesse_base * (1 - coeff))
-        droit = vitesse_base
+def spd_mtr_turn(x: int, boost: bool = False):
+    if x > 512:
+        percent = int((x - 512) * 100 / 512)
+    elif x < 512:
+        percent = int((512 - x) * 100 / 512)
     else:
-        gauche = vitesse_base
-        droit = vitesse_base
-
-    if a:
-        i2c.write(0x10, bytes([0x00, 1, gauche]))
-        i2c.write(0x10, bytes([0x02, 1, droit]))
-    else:
-        i2c.write(0x10, bytes([0x00, 0, gauche]))
-        i2c.write(0x10, bytes([0x02, 0, droit]))
+        return 255 if boost else 100
+    return 255 - percent if boost else 100 - percent
 
 while True:
     rd_msg = rd.receive()
     if rd_msg is not None:
         try:
-            msg = rd_décode(rd_msg)
+            # convertion du message en valeur
+            msg = rd_decode(rd_msg)
             if msg[1] == MON_GAMEPAD or msg[1] == ARBITRE:
+                boost = False
                 values = msg[3].strip('[]').split(',')
                 x = int(values[0].strip())
                 b = values[2].strip() == "True"
                 a = values[1].strip() == "True"
+                # Confirmation de la connection 
                 display.show("A" if a else ".")  
-                moteurs(x, b, a)
-        except:
-            pass
+                # vitesse normal
+                spd = 100
+                # vitesse boost
+                if boost:
+                    spd = 255
+                # Ni A ou B est pressé (arret)
+                if not b and not a:
+                    i2c.write(ADDR, bytes([0x00, 0, 0]))
+                    i2c.write(ADDR, bytes([0x02, 0, 0]))
+                    
+                # Config moteur en fonction de joystick en marche arrière            
+                # Si A pressé (reculer)
+                if a:
+                    if x < 512:
+                        i2c.write(ADDR, bytes([0x00, 2, spd_mtr_turn(x, boost)]))
+                        i2c.write(ADDR, bytes([0x02, 2, spd]))
+                    elif x > 512:
+                        i2c.write(ADDR, bytes([0x00, 2, spd]))
+                        i2c.write(ADDR, bytes([0x02, 2, spd_mtr_turn(x, boost)]))
+                    else:
+                        i2c.write(ADDR, bytes([0x00, 2, spd]))
+                        i2c.write(ADDR, bytes([0x02, 2, spd]))
+                # Config moteur en fonction joystick en marche avant
+                # Si b préssé (avancé)
+                if b:
+                    if x < 512:
+                        i2c.write(ADDR, bytes([0x00, 1, spd_mtr_turn(x, boost)]))
+                        i2c.write(ADDR, bytes([0x02, 1, spd]))
+                    elif x > 512:
+                        i2c.write(ADDR, bytes([0x00, 1, spd]))
+                        i2c.write(ADDR, bytes([0x02, 1, spd_mtr_turn(x, boost)]))
+                    else:
+                        i2c.write(ADDR, bytes([0x00, 1, spd]))
+                        i2c.write(ADDR, bytes([0x02, 1, spd]))
+            sleep(50)
+             
+        except Exception as e:
+            display.scroll(str(e))  # affiche l'erreur sur la matrice
